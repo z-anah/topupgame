c1a05d2a328bed068650a3dc1ccffa3b7fbaa16b
anah <40556052+z-anah@users.noreply.github.com>
2023-08-30 17:26:22 +0700

    dist

---
+++ dist/js/about.fd76925c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktopup"]=self["webpackChunktopup"]||[]).push([[443],{833:function(n,u,t){t.r(u),t.d(u,{default:function(){return f}});var a=t(3396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var o=t(89);const i={},p=(0,o.Z)(i,[["render",c]]);var f=p}}]);
+//# sourceMappingURL=about.fd76925c.js.map
